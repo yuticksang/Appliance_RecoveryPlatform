3dfabcb0843c583d8b9f29466ca2545a9ca7b4d6
yuticksang <yuticksang@gmail.com>
2025-11-04 17:05:14 +0800

    new

--- src/main.server.ts
+++
@@ -1,8 +0,0 @@
-import { BootstrapContext, bootstrapApplication } from '@angular/platform-browser';
-import { App } from './app/shared/app';
-import { config } from './app/shared/app.config.server';
-
-const bootstrap = (context: BootstrapContext) =>
-    bootstrapApplication(App, config, context);
-
-export default bootstrap;
