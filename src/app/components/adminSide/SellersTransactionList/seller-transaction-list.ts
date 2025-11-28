b2e053adf959ed4ffd55ec78076df8e57ea26330
Csyyy <shiyingchua0614@gmail.com>
2025-11-28 23:02:15 +0800

    remove unused route

--- src/app/components/adminSide/SellersTransactionList/seller-transaction-list.ts
+++ src/app/components/adminSide/SellersTransactionList/seller-transaction-list.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { Router, RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
 import { AlertService } from '../../../services/alert.service';
 import { TransactionService, Transaction } from '../../../services/transaction.service';
 import { BreadcrumbComponent } from '../../../shared/breadcrumb/breadcrumb';
@@ -9,7 +9,7 @@ import { BreadcrumbComponent } from '../../../shared/breadcrumb/breadcrumb';
 @Component({
   selector: 'app-seller-transaction-list',
   standalone: true,
-  imports: [CommonModule, FormsModule, RouterLink, BreadcrumbComponent],
+  imports: [CommonModule, FormsModule, BreadcrumbComponent],
   templateUrl: './seller-transaction-list.html',
   styleUrls: ['./seller-transaction-list.scss']
 })
